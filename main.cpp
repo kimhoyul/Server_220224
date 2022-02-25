17887ab98f7f634357719840cde828277103c840
kimhoyul <96504568+kimhoyul@users.noreply.github.com>
2022-02-25 09:40:26 +0900

    Add files via upload
    퓨어한 에코서버

--- main.cpp
+++ main.cpp
@@ -1,39 +1,35 @@
 #include <WinSock2.h>
 #include <iostream>
+#include <string>
 
-using namespace std;
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
 
 #pragma comment(lib, "ws2_32.lib")
 
+using namespace std;
+
 int main()
 {
-	WSAData wsaData;
-
-	//1.winsock 초기화, 윈도우만 합니다. linux, unix에선 사용하지 않아요.
-	if (WSAStartup(MAKEWORD(2, 2), &wsaData))
+	WSAData WasData;
+	WSAStartup(MAKEWORD(2, 2), &WasData);
+	// 1.winsock 초기화
+	if (WSAStartup(MAKEWORD(2, 2), &WasData));
 	{
-		cout << "Winsock Error : " << GetLastError() <<  endl;
+		cout << "Winsock Error : " << GetLastError() << endl;
 		exit(-1);
 	}
 
-	//2.create socket
+	//2. 리슨소켓 생성
 	SOCKET ServerSocket;
-
-	ServerSocket = socket(
-		AF_INET,
-		SOCK_STREAM,
-		0
-	);
-
+	ServerSocket = socket(AF_INET, SOCK_STREAM, 0);
 	if (ServerSocket == INVALID_SOCKET)
 	{
-		cout << "socket Error : " << GetLastError() << endl;
+		cout << "SOCKET Error : " << GetLastError() << endl;
 		exit(-1);
 	}
 
-	//2. ip, port를 할당해서 소켓이랑 연결
+	//3.IP, PORT 를 생성한 소켓에 할당
 	SOCKADDR_IN ServerAddr;
-
 	memset(&ServerAddr, 0, sizeof(ServerAddr));
 	ServerAddr.sin_family = AF_INET;
 	ServerAddr.sin_port = htons(50000);
@@ -41,38 +37,33 @@ int main()
 
 	if (bind(ServerSocket, (SOCKADDR*)&ServerAddr, sizeof(ServerAddr)) == SOCKET_ERROR)
 	{
-		cout << "bind Error : " << GetLastError() << endl;
-		exit(-1);
-	}
-
-	//3. 클라이언트 연결을 기다려라.
-	if (listen(ServerSocket, 0) == SOCKET_ERROR)
-	{
-		cout << "listen Error : " << GetLastError() << endl;
-		exit(-1);
+	cout << "Bind Error :  " << GetLastError() << endl;
+	exit(-1);
 	}
 
 	//4. 수락해서 클라이언트와 연결을 완성
 	SOCKADDR_IN ClientAddr;
-	int ClinetAddrSize = sizeof(ClientAddr);
-	SOCKET ClientSocket = 0;
-	ClientSocket = accept(ServerSocket, (SOCKADDR*)&ClientAddr, &ClinetAddrSize);
+	int ClinteAddrSize = sizeof(ClientAddr);
+	SOCKET ClientSocket;
+	ClientSocket = accept(ServerSocket, (SOCKADDR*)&ClientAddr, &ClinteAddrSize);
 	if (ClientSocket == SOCKET_ERROR)
 	{
 		cout << "accept Error : " << GetLastError() << endl;
 		exit(-1);
 	}
+	
+	//5. Send
+	char Buffer[] = "Hellow World";
+	recv(ClientSocket, Buffer, 1024, 0);
+	send(ClientSocket, Buffer, strlen(Buffer) + 1, 0);
+	cout << "client send : " << Buffer << endl;
 
-	//5. 보낸다 자료를 클라이언트한테
-	char Message[] = "Hello World"; //'\0'
-	send(ClientSocket, Message, strlen(Message) + 1, 0);
-
-	//6. 연결 종료
+	//6. 연결종료
 	closesocket(ClientSocket);
 	closesocket(ServerSocket);
 
-	//7.Winsock 종료, 윈도우만 합니다.
+	//7. Winsock 종료
 	WSACleanup();
-
+	
 	return 0;
 }
